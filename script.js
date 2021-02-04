91754e21c1a091fa6849fa7746c0634e96318a02
Sarah <sara616b@stud.kea.dk>
2021-02-04 12:33:54 +0100

    initial- docs from teacher plus script.js 'outline' created

---
+++ script.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", start);
+
+function start() {
+  console.log("start");
+}
+
+function getUserSelection() {
+  console.log("getUserSelection");
+}
+
+function makeRandomComputerChoice() {
+  console.log("makeRandomComputerChoice");
+}
+
+function showAnimations() {
+  console.log("showAnimations");
+}
+
+function determineWinner() {
+  console.log("determineWinner");
+}
+
+function showResult() {
+  console.log("showResult");
+}
+
+function askPlayAgain() {
+  console.log("playAgain");
+}
